07058787adc0832d731bf04f50b5d798837b41c6
akuleshov7 <andrewkuleshov7@gmail.com>
2025-01-03 23:30:45 +0300

    Hotfix for publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version ("2.1.0")
     java
+    id("com.akuleshov7.buildutils.publishing-configuration")
     // id("com.akuleshov7.vercraft.plugin-gradle") version("0.0.1")
 }
