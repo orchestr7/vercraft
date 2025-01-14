e8afc6fe17bf0424758a159664002276d39cd8a9
akuleshov7 <andrewkuleshov7@gmail.com>
2025-01-14 23:48:18 +0300

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,3 @@ plugins {
     id("com.akuleshov7.buildutils.publishing-configuration")
     // id("com.akuleshov7.vercraft.plugin-gradle") version("0.0.2")
 }
-
-println("=== ${System.getenv("GITHUB_HEAD_REF")}")
-println("=== ${System.getenv("GITHUB_REF_NAME")}")
-println("=== ${System.getenv("GITHUB_REF")}")
