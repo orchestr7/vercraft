36fd1fdf4dcefa9b2d1b9b744e0fb8fc220c9a26
akuleshov7 <andrewkuleshov7@gmail.com>
2025-02-23 23:37:55 +0300

    Adding the support "X" in patch versions instead of 0
    ### What's done:
    - now versions are calculated with X in patch version, for example: 1.1.x. This will make branch naming more clear.
    - polishing tests

--- core/src/test/kotlin/com/akuleshov7/vercraft/tests/LatestBranchTest.kt
+++ core/src/test/kotlin/com/akuleshov7/vercraft/tests/LatestBranchTest.kt
@@ -13,7 +13,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 const val DETACHED_COMMIT_RIGHT_AFTER_RELEASE_IN_THE_MIDDLE = "df22d05e681404c1ed98b0db0bf041d60236d14c"
-const val DETACHED_COMMIT_AT_1_1_0 = "9f4c9ba873ed8329a8b913df9bf31c7d81671d8b"
+const val DETACHED_COMMIT_AT_1_1_0 = "9e2e23d82db76a5b6f1aee8a2bb8ffaf485ee9a0"
 const val DETACHED_COMMIT_WITHOUT_RELEASE_BEFORE_IT = "84f4bf70c9ca4da3f6e253d5c206159838ab2522"
 
 class LatestBranchTest {
