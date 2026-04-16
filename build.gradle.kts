500206b415e69b1ff9a3951b86e5f4010d8c8d47
Denis Markushin <denis.markushin@gmail.com>
2026-04-16 12:03:36 +0300

    fix: Kotlin metadata version incompatibility with Gradle's embedded Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 project.description = "Versioning tool for real enterprise"
 
 plugins {
-    kotlin("jvm") version ("2.3.10")
+    alias(libs.plugins.kotlin.jvm)
     java
     id("com.akuleshov7.buildutils.publishing-configuration")
     id("com.akuleshov7.vercraft.plugin-gradle") version("0.6.0")
